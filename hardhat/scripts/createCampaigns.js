373e60779afdf0d426327bcddfbbc40608587c75
Giuseppe Marotta <piccolino96@gmail.com>
2022-10-25 13:13:50 -0500

    feat: updated contracts

--- hardhat/scripts/createCampaigns.js
+++ hardhat/scripts/createCampaigns.js
@@ -1,10 +1,12 @@
 const { ethers } = require("hardhat");
 async function main() {
+  // Replace your contract factory address Below
+  var contractFactoryAddr = "0xe87d5061cC1CC801404D5686C7C4c0819aA1059F"
   const contract = await ethers.getContractAt(
     "CrowdFactory",
-    "0x2e59b8f50eA64E110Ce86417a4d5d0eD91740F7e"
+    contractFactoryAddr
   );
-  stock = [5, 5, 5, 5, 5, 5, 5, 5];
+  stock = [2,2,2,2,2,2,2,2];
   standar_cost = ethers.utils.parseUnits("0.1", 18);
   premium_cost = ethers.utils.parseUnits("0.2", 18);
   cost = [
@@ -20,9 +22,9 @@ async function main() {
   //projOwnerAddr
   projOwnerAddr = "0x007a5203F27a5AC24F9B6C7e635b76FE35059A60";
   await contract.createProject(
-    "Serati Vibes 2",
-    "Evento en homenaje a Serati 2.",
-    ethers.utils.parseUnits("10.0", 18),
+    "Serati Vibes 3",
+    "Evento en homenaje a Serati 3.",
+    ethers.utils.parseUnits("100.0", 18),
     stock,
     cost,
     projOwnerAddr
